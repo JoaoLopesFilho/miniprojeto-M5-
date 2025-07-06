530ce32f754314b04feaebe34671f6837cf6e850
JoaoLopesFilho <lopesjoaofilho@gmail.com>
2025-07-05 21:12:18 -0300

    baixando tudo que é necessario

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Button from "./components/button/button";
+
+export default function Home() {
+  return (
+    <>
+
+    <h1>abdbjdbf</h1>
+
+    <Button></Button>
+    </>
+  );
+}
