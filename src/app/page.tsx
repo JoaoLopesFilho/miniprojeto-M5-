f813f665c821a2ecbc29aeb603397a1dd0eddfc0
joão lopes <lopesjoaofilho670@gmail.com>
2025-07-08 18:56:32 -0300

    organizando o body do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,35 +6,32 @@ import SearchButton from "./components/SearchButton";
 
 export default function Home() {
   return (
-    <>
-    
+     <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
       <Header />
 
-       <main style={{
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-      paddingBottom: '50px', 
-    }}>
-      
-      <section style={{
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        gap: '20px'
-      }}>
-        
-        <h2>O que vai ser hoje?</h2>
-        <SearchButton />
-      </section>
-
+      <main style={{ flex: 1 }}>
+        <section
+          style={{
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            gap: '20px',
+          }}
+        >
+          <h2>O que vai ser hoje?</h2>
+          <SearchButton />
+        </section>
+      </main>
 
       <Footer />
-    </main>
-  );
-    
-    </>
+    </div>
   );
 }
